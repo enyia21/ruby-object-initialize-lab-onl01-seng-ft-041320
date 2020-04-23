520e8b3d9cd0af18ac17dfb8ee11e0208781a250
Chibuisi Enyia <enyia21@gmail.com>
2020-04-23 04:18:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,14 @@
 # require 'pry'
 class Dog
+  attr_writter :breed = "Mutt"
   def initialize(name, breed)
     @name = name
     # binding.pry
     @breed = breed
+  end
+
+  def breed
+    
   end
 end
 
